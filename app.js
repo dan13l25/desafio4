99400730e2ce3e060eecc07a642cd66bc73944e0
Daniel Alemán <dan13laleman14@gmail>
2024-03-03 20:42:14 -0300

    lista de producto

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import express, { urlencoded } from "express"
+import express from "express"
 import __dirname from "./utils.js"
 import handlebars from "express-handlebars"
 import { Server } from "socket.io"
@@ -13,15 +13,27 @@ app.set('views', `${__dirname}/views`)
 app.set('view engine', 'handlebars')
 app.engine('handlebars', handlebars.engine())
 
-app.get("/", (req,res)=>{
-    const datos = {
-        mensaje: "Hola desde el servidor",
-        otroDato: "Información adicional"
-    };
+app.get("/products", (req, res) => {
+  // Aquí se debe obtener la lista de productos desde la base de datos
+  const products = [
+    { id: 1, name: "Producto 1" },
+    { id: 2, name: "Producto 2" },
+    { id: 3, name: "Producto 3" },
+  ];
 
-    res.render("realtimeproducts", datos);
+  // Se envía la lista de productos como respuesta
+  res.json(products);
 });
 let server = app.listen(port, () => {
-    console.log("Servidor funcionando en http://localhost:" + port);
-});
+    console.log("Servidor funcionando en http://localhost:" + port)
+})
+
 const io = new Server (server)  
+
+io.on('connection', socket => {
+    console.log("Conectado");
+    
+    socket.on("message", data => {
+        io.emit('log', data);
+    })
+})
--- public/js/realTimeProducts.js
+++ public/js/realTimeProducts.js
@@ -1,1 +1,21 @@
-console.log("prueba")
+function getProducts() {
+    fetch("/products")
+      .then((response) => response.json())
+      .then((products) => {
+        // Aquí se debe actualizar la lista de productos en la vista
+        const productList = document.getElementById("productList");
+        productList.innerHTML = "";
+  
+        products.forEach((product) => {
+          const productElement = document.createElement("li");
+          productElement.textContent = product.name;
+          productList.appendChild(productElement);
+        });
+      });
+  }
+  
+  // Se realiza la primera solicitud de Long Polling
+  getProducts();
+  
+  // Se configura un intervalo para realizar las solicitudes de Long Polling cada 5 segundos
+  setInterval(getProducts, 5000);
