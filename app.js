ae35f0a114a978533407a06a969fd74704d3f28e
Daniel Alemán <dan13laleman14@gmail>
2024-03-03 00:57:34 -0300

    handlebar vizualisado

--- app.js
+++ app.js
@@ -8,5 +8,20 @@ const port = 8080
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+app.use(express.static(__dirname + '/public'))
+app.set('views', `${__dirname}/views`)  
+app.set('view engine', 'handlebars')
+app.engine('handlebars', handlebars.engine())
 
-app.listen(console.log("funcionando en servidor", port))
+app.get("/", (req,res)=>{
+    const datos = {
+        mensaje: "Hola desde el servidor",
+        otroDato: "Información adicional"
+    };
+
+    res.render("realtimeproducts", datos);
+});
+let server = app.listen(port, () => {
+    console.log("Servidor funcionando en http://localhost:" + port);
+});
+const io = new Server (server)  
